d2fe4b6f3d94ed687d45e6db61a5b8b62d5a84ea
YonatanBiton <yuyu82114@gmail.com>
2021-02-11 18:38:09 +0200

    Added multiple file functionalty

--- Server Side/Server.py
+++ Server Side/Server.py
@@ -7,21 +7,21 @@ UPLOAD_FOLDER = 'Server Side\Dataset\\'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/', methods=['GET'])
 def get_index():
     html_file = open(f'{os.getcwd()}\Client Side\client.html', 'r')
     return html_file.read()
 
+
 @app.route('/', methods=['POST'])
 def post_index():
-    if 'midFile' not in request.files:
-        return redirect(request.url)
-    midi_file = request.files['midFile']
-    print(midi_file)
-    if midi_file.filename.endswith('.mid'):
-        filename = midi_file.filename
-        midi_file.save(f'{UPLOAD_FOLDER}{filename}')
+    for midi_file in request.files.getlist('midFile'):
+        if midi_file.filename.endswith('.mid'):
+            filename = midi_file.filename
+            midi_file.save(f'{UPLOAD_FOLDER}{filename}')
     return redirect(request.url)
+
 
 @app.route('/learn', methods=['POST'])
 def post_learn():
