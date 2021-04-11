ff9d951d435510a7a3c8fb55c5fbd536d1a23362
Ido Arbiv <devgamesil15@gmail.com>
2021-04-11 21:24:21 +0300

    Removed unused models

--- Server Side/Models/Note.py
+++
@@ -1,7 +0,0 @@
-class Note:
-    def __init__(self, type, when_note_starts, time):
-        self.type = type
-        self.when_note_starts = when_note_starts
-        self.time = time
-    def __str__(self):
-        return f"Note(type={self.type}, when_note_starts={self.when_note_starts}, time={self.time})"
